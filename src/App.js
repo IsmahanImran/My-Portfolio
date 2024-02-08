5085e2c72e0361d5f3a03a046e33ebca86c876d6
Ismahan Imran <ismahanimran9@gmail.com>
2024-02-08 06:21:15 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar.js';
+import Home from './Home.js';
+import Projects from './Projects.js';
+import Contact from './Contact.js';
+import Footer from './Footer.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Navbar />
+      <div className = "container">
+        <Home />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
     </div>
   );
 }
